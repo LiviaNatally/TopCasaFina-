8b1d502a8e39d1df326ea0d9cff41b08f72af31c
LiviaNatally <104009116+LiviaNatally@users.noreply.github.com>
2022-06-20 10:42:43 -0300

    Add files via upload

---
+++ js/navbar-animation-fix.js
@@ -0,0 +1,8 @@
+$('#navbar-collapse').on('show.bs.collapse', function() {
+	$('.topCasaFina-banner').css('transform', 'translate(-50%, 10%');
+
+});
+
+$('#navbar-collapse').on('hide.bs.collapse', function() {
+	$('.topCasaFina-banner').css('transform', 'translate(-50%, -50%');
+});
